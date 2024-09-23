d0615da087bc58b165dbca5e72e97708ecab1722
Rick Grice <npopbwls@students.codeinstitute.net>
2024-09-23 11:23:09 +0000

    Added Functitions to Java Script

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+function eungame () {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditiononQuestion() {
+
+}
+
+function displySubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+
+}
