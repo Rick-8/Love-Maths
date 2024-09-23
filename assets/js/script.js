025dadb40468282ecc59d7da7607df038cf04152
Rick Grice <npopbwls@students.codeinstitute.net>
2024-09-23 12:09:39 +0000

    Added Alerts to the buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,22 @@
-function eungame () {
+// Wait for the DOM to finish loading before running the game
+// Get the button elements and add event listeners to them
+
+document.addEventListener("DOMContentLoaded", function () {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("you clicked Submit!");
+            } else {
+                let gametype = this.getAttribute("data-type");
+                alert(`You Clicked ${gametype}`);
+            }
+        })
+    }
+})
+
+function rungame() {
 
 }
 
